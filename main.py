a161b5573d0d9ae6e7e70aae4a57202dab3ace1f
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-27 01:51:03 -0700

    hw3-5

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
     Code Your Program here
     ########################################
     """
+    #condition1:must start with the alphabet character ( ‘a~z’, ‘A~z’) 
+    #condition2:email string length is greater than 5 and less than 30
+    #condition3:It must include the letter '@' and must include at least one '.' after ‘@’
+
+    
+    
+    
     result = True
 
     ########################################
