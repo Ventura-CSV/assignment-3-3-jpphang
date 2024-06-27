446cf3d01043279ff176d32f4aa51533895a919d
jpphang <joyanna_phang1@my.vcccd.edu>
2024-06-27 02:23:14 -0700

    3-5

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
-def main(email):
+def main():
     email = input('Enter your email: ')
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-    #condition1:must start with the alphabet character ( ‘a~z’, ‘A~z’) 
-    if not email [0]. isalpha():
+    
+    # condition1: must start with the alphabet character ( ‘a~z’, ‘A~z’)
+    if not email[0].isalpha():
         return False
     
-    #condition2:email string length is greater than 5 and less than 30
-    if len() <= 5 and len() >=30:
+    # condition2: email string length is greater than 5 and less than 30
+    if len(email) <= 5 or len(email) >= 30:
         return False
     
-    #condition3:It must include the letter '@' and must include at least one '.' after ‘@’
-    at_index = email. find('@')
+    # condition3: It must include the letter '@' and must include at least one '.' after ‘@’
+    at_index = email.find('@')
     if at_index == -1:
         return False
     
-    dot_index = email. find('.', at_index)
+    dot_index = email.find('.', at_index)
     if dot_index == -1:
         return False
     
-        return True
-    
-    
-    result = main(email)
-
-    print(result)
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    return result
-
+    return True
 
 if __name__ == '__main__':
-    main()
+    result = main()
+    print(result)
